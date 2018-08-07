89230e3a0cedefd7fa8a2c4b0806b8105571bd89
Da Woon Lee <daniel.lee9415@gmail.com>
2018-08-07 16:53:02 -0400

    changed title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Menu } from 'semantic-ui-react'
 export default function Navbar() {
   return (
     <Menu inverted compact borderless attached widths={3}>
-      <Menu.Item>Spotify Stage Two Assessment</Menu.Item>
+      <Menu.Item></Menu.Item>
       <Menu.Item>Schedu-Lee</Menu.Item>
       <Menu.Item>Da Woon Lee</Menu.Item>
     </Menu>
