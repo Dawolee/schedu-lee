5ae1f3f0f04659b723a7e5d822410ccda2a6c849
Da Woon Lee <daniel.lee9415@gmail.com>
2018-08-07 17:48:46 -0400

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import { Menu } from 'semantic-ui-react'
 export default function Navbar() {
   return (
     <Menu inverted compact borderless attached widths={3}>
-      <Menu.Item>Da Woon Lee</Menu.Item>
+      <Menu.Item />
       <Menu.Item>Schedu-Lee</Menu.Item>
-      <Menu.Item>Test</Menu.Item>
+      <Menu.Item />
     </Menu>
   )
 }
